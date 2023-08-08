674626e0a01d953ab605bd670e3f5abcd439656c
shrvr <shurvir04@gmail.com>
2023-08-08 11:19:34 -0400

    refactor(apis, root): ref the config.ts file [SCRUM-6]

--- tys-apis/src/config.ts
+++ tys-apis/src/config.ts
@@ -1,14 +1,11 @@
+/* eslint-disable no-process-env */
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// eslint-disable-next-line no-process-env
 const PORT = process.env.PORT;
-// eslint-disable-next-line no-process-env
 const MONGO_URL = process.env.MONGO_URL;
-// eslint-disable-next-line no-process-env
 const PASS_SALT = process.env.PASS_SALT;
-// eslint-disable-next-line no-process-env
 const NODE_ENV = process.env.NODE_ENV;
 
 export default {
