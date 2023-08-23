29ca6f67c4a4e6c29f0b36c8cdc686c484643500
shrvr <shurvir04@gmail.com>
2023-08-23 12:16:15 -0400

    chore(api): add cors middleware [SCRUM-18]

--- tys-apis/src/index.ts
+++ tys-apis/src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -16,6 +17,7 @@ if (config.NODE_ENV !== 'test') {
     });
 }
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoute);
 
